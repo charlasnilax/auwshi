2c2269402ddf5f4aa63e597ab4fd388ceff97c7a
codesantaisai <jathurshan@protonmail.com>
2023-05-28 21:00:12 +0530

    2023.05.28

--- frontend/src/Homepage/home.js
+++ frontend/src/Homepage/home.js
@@ -27,10 +27,10 @@ const Loader = () => {
 const CarouselComponent = () => {
   return (
     
-    <div className="container-fluid p-2 mb-5 wow fadeIn firstcaro" data-wow-delay="0.1s" >
-      <Carousel id="header-carousel firstcaro" style = {{}}>
+    <div className="container-fluid p-2 mb-5 wow fadeIn carousel-wrapper" data-wow-delay="0.1s" >
+      <Carousel id="header-carousel" style = {{}}>
         <Carousel.Item>
-          <img className="w-100 firstcaro" src="images/products/imagev3.jpg" alt="Image" />
+          <img className="w-100" src="images/products/imagev3.jpg" alt="Image" />
           <Carousel.Caption>
             <div className="container">
               <div className="row justify-content-start">
@@ -67,7 +67,7 @@ const About = () => {
   return (
     <div className="container-xxl py-5">
       <div className="container">
-        <div className="row g-5 align-items-center">
+        <div className="row g-5 align-items-center same">
           <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
             <div className="about-img position-relative overflow-hidden p-5 pe-0">
               <img className="img-fluid w-100" src="images/products/imageV1.jpg" alt="About" />
@@ -88,7 +88,7 @@ const About = () => {
 
 const Feature = () => {
   return (
-    <div className="container-fluid bg-light bg-icon my-5 py-6">
+    <div className="container-fluid ourfeature bg-icon my-5 py-6">
       <div className="container">
         <div className="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style={{ maxWidth: '500px' }}>
           <h1 className="display-5 mb-3">Our Features</h1>
@@ -129,7 +129,7 @@ const Feature = () => {
 function ProductSection() {
 return (
   
-  <div className="container-xxl py-5">
+  <div className="container-xxl py-5 Hprodut">
     <div className="container">
       <div className="row g-0 gx-5 align-items-end">
         <div className="col-lg-6">
@@ -168,24 +168,16 @@ Some of Auwshi's Products
                   <a className="d-block h5 mb-2" href="">
                     Dry-Ginger
                   </a>
-                  {/* <span className="text-primary me-1">$19.00</span>
-                  <span className="text-body text-decoration-line-through">
-                    $29.00
-                  </span> */}
+                 
                 </div>
                 <div className="d-flex border-top">
-                  <small className="w-50 text-center border-end py-2">
-                    <a className="text-body" href="">
+                  <small className="w-100 text-center border-end py-2">
+                    <a className="text-body" href="/product">
                       <i className="fa fa-eye text-primary me-2" />
                       View detail
                     </a>
                   </small>
-                  <small className="w-50 text-center py-2">
-                    <a className="text-body" href="">
-                      <i className="fa fa-shopping-bag text-primary me-2" />
-                      Add to cart
-                    </a>
-                  </small>
+                  
                 </div>
               </div>
             </div>
@@ -205,7 +197,7 @@ Some of Auwshi's Products
                   </div>
                 </div>
                 <div className="text-center p-4">
-                  <a className="d-block h5 mb-2" href="">
+                  <a className="d-block h5 mb-2" href="/product">
                     Tooth Powder
                   </a>
                   {/* <span className="text-primary me-1">$19.00</span>
@@ -214,18 +206,13 @@ Some of Auwshi's Products
                   </span> */}
                 </div>
                 <div className="d-flex border-top">
-                  <small className="w-50 text-center border-end py-2">
-                    <a className="text-body" href="">
+                  <small className="w-100 text-center border-end py-2">
+                    <a className="text-body" href="/product">
                       <i className="fa fa-eye text-primary me-2" />
                       View detail
                     </a>
                   </small>
-                  <small className="w-50 text-center py-2">
-                    <a className="text-body" href="">
-                      <i className="fa fa-shopping-bag text-primary me-2" />
-                      Add to cart
-                    </a>
-                  </small>
+                  
                 </div>
               </div>
             </div>
@@ -248,24 +235,16 @@ Some of Auwshi's Products
                   <a className="d-block h5 mb-2" href="">
                     Hair Oil
                   </a>
-                  {/* <span className="text-primary me-1">$19.00</span>
-                  <span className="text-body text-decoration-line-through">
-                    $29.00
-                  </span> */}
+                  
                 </div>
                 <div className="d-flex border-top">
-                  <small className="w-50 text-center border-end py-2">
-                    <a className="text-body" href="">
+                  <small className="w-100 text-center border-end py-2">
+                    <a className="text-body" href="/product">
                       <i className="fa fa-eye text-primary me-2" />
                       View detail
                     </a>
                   </small>
-                  <small className="w-50 text-center py-2">
-                    <a className="text-body" href="">
-                      <i className="fa fa-shopping-bag text-primary me-2" />
-                      Add to cart
-                    </a>
-                  </small>
+                  
                 </div>
               </div>
             </div>
@@ -288,31 +267,23 @@ Some of Auwshi's Products
                   <a className="d-block h5 mb-2" href="">
                     Sikaikai Powder
                   </a>
-                  {/* <span className="text-primary me-1">$19.00</span>
-                  <span className="text-body text-decoration-line-through">
-                    $29.00
-                  </span> */}
+                  
                 </div>
                 <div className="d-flex border-top">
-                  <small className="w-50 text-center border-end py-2">
-                    <a className="text-body" href="">
+                  <small className="w-100 text-center border-end py-2">
+                    <a className="text-body" href="/product">
                       <i className="fa fa-eye text-primary me-2" />
                       View detail
                     </a>
                   </small>
-                  <small className="w-50 text-center py-2">
-                    <a className="text-body" href="">
-                      <i className="fa fa-shopping-bag text-primary me-2" />
-                      Add to cart
-                    </a>
-                  </small>
+            
                 </div>
               </div>
             </div>
            
           </div>
         </div>
-        <div id="tab-2" className="tab-pane fade show p-0">
+        {/* <div id="tab-2" className="tab-pane fade show p-0">
           <div className="row g-4">
             <div className="col-xl-3 col-lg-4 col-md-6">
               <div className="product-item">
@@ -426,7 +397,7 @@ Some of Auwshi's Products
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   </div>)
--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -1,3 +1,8 @@
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+
+
 
 export  default function Footer (){
     return (
@@ -9,7 +14,7 @@ export  default function Footer (){
   >
     <div className="container py-5">
       <div className="row g-5">
-        <div className="col-lg-3 col-md-6">
+        <div className="col-lg-4 col-md-6">
           <h1 className="fw-bold text-primary mb-4">
             A<span className="text-secondary">uw</span>shi
           </h1>
@@ -19,38 +24,40 @@ export  default function Footer (){
           <div className="d-flex pt-2">
             <a
               className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
+              href="https://www.twitter.com/auwshiherbs"
             >
               <i className="fab fa-twitter" />
             </a>
             <a
               className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
+              href="https://www.facebook.com/auwshiherbs"
             >
               <i className="fab fa-facebook-f" />
             </a>
             <a
               className="btn btn-square btn-outline-light rounded-circle me-1"
-              href=""
+              href="https://www.youtube.com/auwshiherbs"
             >
               <i className="fab fa-youtube" />
             </a>
             <a
               className="btn btn-square btn-outline-light rounded-circle me-0"
-              href=""
+              href="https://www.linkedin.com/auwshiherbs"
             >
               <i className="fab fa-linkedin-in" />
             </a>
           </div>
         </div>
-        <div className="col-lg-3 col-md-6 address">
+        <div className="col-lg-4 col-md-6 address">
           <h4 className="text-dark mb-4 ">Address</h4>
           <p>
-            <i className=" fa fa-map-marker-alt me-3" />
+          <FontAwesomeIcon icon={faMapMarkerAlt} className="me-3" />
+
           Chankanai, Vikneshwara road,No:09
           </p>
           <p>
-            <i className="fa fa-phone-alt me-3" />
+          <FontAwesomeIcon icon={faPhoneAlt} className="me-3" />
+
             +94 706443471
           </p>
           <p>
@@ -58,43 +65,25 @@ export  default function Footer (){
             auwshiherbs@gmai
           </p>
         </div>
-        <div className="col-lg-3 col-md-6">
+        <div className="col-lg-4 col-md-6">
           <h4 className="text-dark mb-4">Quick Links</h4>
           <a className="btn btn-link" href="/aboutus">
             About Us
           </a>
-          {/* <a className="btn btn-link" href="">
-            Contact Us
-          </a> */}
+         
           <a className="btn btn-link" href="">
             Our Services
           </a>
-          {/* <a className="btn btn-link" href="">
-            Terms &amp; Condition
-          </a>
-          <a className="btn btn-link" href="">
-            Support
-          </a> */}
+          
         </div>
-        {/* <div className="col-lg-3 col-md-6">
-          <h4 className="text-light mb-4">Newsletter</h4>
-          <p>Dolor amet sit justo amet elitr clita ipsum elitr est.</p>
-          <div className="position-relative mx-auto" style={{ maxWidth: 400 }}>
-            <input
-              className="form-control bg-transparent w-100 py-3 ps-4 pe-5"
-              type="text"
-              placeholder="Your email"
-            />
-           
-          </div>
-        </div> */}
+       
       </div>
     </div>
     <div className="container-fluid copyright">
       <div className="container">
         <div className="row">
-          <div className="col-md-6 text-center text-md-start mb-3 mb-md-0">
-            © <a href="#">Auwshi Herbs</a>, All Right Reserved.
+          <div className="col-md-12 text-center text-dark mb-3 mb-md-0">
+            ©Auwshi Herbs, All Right Reserved.2023
           </div>
          
         </div>
