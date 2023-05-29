b54b00caa47b491437e3bde79743f50f9ad86e36
codesantaisai <jathurshan@protonmail.com>
2023-05-29 11:20:29 +0530

    map deleted and new one added

--- frontend/src/AboutUs/about.js
+++ frontend/src/AboutUs/about.js
@@ -7,8 +7,8 @@ import './bootstrap.min.css';
 import './style.css';
 import './animate.css';
 import './animate.min.css';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import MapContainer from './Map';
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
+// import MapContainer from './Map';
 
 
 
@@ -299,22 +299,26 @@ function Foody(){
       {/* contact End */}
 
       
-      {/* Google Map Start */}
-
-
-    
-      {/* Other content */}
-      <MapContainer />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-  
+      <>
+  {/* Google Map Start */}
+  <div
+    className="container-xxl px-0 wow fadeIn"
+    data-wow-delay="0.1s"
+    style={{ marginBottom: "-6px" }}
+  >
+    <iframe
+      className="w-100"
+      style={{ height: 450 }}
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3001156.4288297426!2d-78.01371936852176!3d42.72876761954724!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4ccc4bf0f123a5a9%3A0xddcfc6c1de189567!2sNew%20York%2C%20USA!5e0!3m2!1sen!2sbd!4v1603794290143!5m2!1sen!2sbd"
+      frameBorder={0}
+      allowFullScreen=""
+      aria-hidden="false"
+      tabIndex={0}
+    />
+  </div>
   {/* Google Map End */}
+</>
+
   <link href="css/bootstrap.min.css" rel="stylesheet" />
       <link href="css/style.css" rel="stylesheet" />
         {/* Footer Start */}
@@ -371,7 +375,7 @@ function Foody(){
           </p>
           <p>
             <i className="fa fa-envelope me-3" />
-            info@example.com
+            info@example.comgit 
           </p>
         </div>
         <div className="col-lg-3 col-md-6">
